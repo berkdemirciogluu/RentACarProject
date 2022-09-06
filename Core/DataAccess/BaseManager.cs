dee4eb489eb2c84668411d981b1d060d8cdb5d2c
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-09-07 02:49:23 +0300

    IBaseService added, BaseManager will be added

---
+++ Core/DataAccess/BaseManager.cs
@@ -0,0 +1,38 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public class BaseManager : IBaseService<T>
+    {
+
+        public IResult Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
